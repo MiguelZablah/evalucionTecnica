44afac10912e07bb4da59f2eaa1c274a11e7bd8d
miguel12979 <miguel12979@gmail.com>
2017-10-10 02:14:46 -0500

    Parte 2 terminada

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,7 @@
-$(document).ready(function () {
-    $("#ciudad").autocomplete({
-        source: function (request, response) {
-            $.ajax({
-                url: "http://ws.geonames.org/searchJSON",
-                dataType: "jsonp",
-                data: {
-                    featureClass: "P",
-                    style: "full",
-                    maxRows: 6,
-                    name_startsWith: request.term
-                },
-                success: function (data) {
-                    response($.map(data.geonames, function (item) {
-                        return {
-                            label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
-                            value: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
-                        }
-                    }));
-                }
-            });
-        },
-        minLength: 3,
-        select: function (event, ui) {
-            log(ui.item ?
-                "Selected: " + ui.item.label :
-                "Nothing selected, input was " + this.value);
-        },
-        open: function () {
-            $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
-        },
-        close: function () {
-            $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
-        }
-    });
-});
+!function (n, t) { "use strict"; var r = n.fn.val; n.fn.val = function (n) { if (!arguments.length) return r.call(this); var e = r.call(this, n); return t.element(this[0]).triggerHandler("input"), e } }(window.jQuery, window.angular);
+
+$.get("http://ipinfo.io", function (response) {
+    var city = response.city;
+    var contry = response.country;
+    $("#ciudad").val(city + ', ' + contry).trigger('input');
+}, "jsonp");
